9535a5c8f5634d1f5de4a50962232ae5f34643ee
Camilo <cacespedes@uninorte.edu.co>
2022-05-23 15:53:43 -0500

    Rutas

--- src/routes/navigate.ts
+++ src/routes/navigate.ts
@@ -1,9 +1,20 @@
 import express from "express"
 const router = express.Router()
 
+router.get('/', (_,res) => {
+    res.render('index.ejs')
+})
 
-router.get('/login-seller',(req,res) =>{
-    res.render('login-seller.ejs')
+router.get('/index', (_,res) => {
+    res.render('index.ejs')
+})
+
+router.get('/blog', (_,res) => {
+    res.render('blog.ejs')
+})
+
+router.get('/blog-single', (_,res) => {
+    res.render('blog-single.ejs')
 })
 
 router.get('/blog1-single',(req,res) =>{
@@ -13,22 +24,30 @@ router.get('/blog1-single',(req,res) =>{
 router.get('/blog2-single',(req,res) =>{
     res.render('blog2-single.ejs')
 })
-router.get('/', (_,res) => {
-    res.render('index.ejs')
+
+router.get('/login', (_,res) => {
+   res.render('login.ejs')
 })
 
-router.get('/index', (_,res) => {
-    res.render('index.ejs')
+router.get('/login-seller',(req,res) =>{
+    res.render('login-seller.ejs')
 })
 
+ router.get('/shop', (_,res) => {
+    res.render('shop.ejs')
+ })
 
-router.get('/blog-single', (_,res) => {
-    res.render('blog-single.ejs')
-})
+router.get('/product-details', (_,res) => {
+    res.render('product-details.ejs')
+ })
 
-router.get('/blog', (_,res) => {
-    res.render('blog.ejs')
-})
+ router.get('/blog1-product-details', (_,res) => {
+    res.render('blog1-product-details.ejs')
+ })
+
+ router.get('/blog2-product-details', (_,res) => {
+    res.render('blog2-product-details.ejs')
+ })
 
 router.get('/cart', (_,res) => {
     res.render('cart.ejs')
@@ -42,18 +61,6 @@ router.get('/contact-us', (_,res) => {
     res.render('contact-us.ejs')
 })
 
-router.get('/login', (_,res) => {
-   res.render('login.ejs')
-})
-
-router.get('/product-details', (_,res) => {
-    res.render('product-details.ejs')
- })
-
- router.get('/shop', (_,res) => {
-    res.render('shop.ejs')
- })
-
  router.get('/perfil', (_,res) => {
     res.render('shop.ejs')
  })
@@ -61,8 +68,5 @@ router.get('/product-details', (_,res) => {
  router.get('/perfil', (_,res) => {
     res.render('login-.ejs')
  })
-
-
- 
 
 export default router
